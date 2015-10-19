a271c50366c79398583c6dbeedd6035e8464b02c
BarabaraLee <linjunli@Linjuns-MacBook-Pro.local>
2015-10-18 21:39:53 -0400

    j

--- HW2_P5.R
+++ HW2_P5.R
@@ -1,4 +1,8 @@
 #Part5
-
+library(glmnet)
 spam.dat<-read.table('spam.data.txt',sep=' ')
-spam.dat$V58<-as.factor(spam.dat$V58)
+y<-as.factor(spam.dat$V58)
+X<-model.matrix(~.,spam.dat[,1:57])
+cv.myNewlogit <- cv.glmnet(X,y,alpha=1,nfolds=10,family='binomial')
+plot(cv.myNewlogit,xvar="lambda")
+cv.myNewlogit$lambda.min #=0.0004034505
